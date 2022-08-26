48a23f8e07393e3329c3fd104c426c44f0fe4865
brayan.castiblanco-b <kmilocastib22@outlook.com>
2022-08-26 18:10:18 -0500

    Third

--- src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class RegistryTest {
     private Registry registry = new Registry();
     @Test
-    public void validateRegistryResult() {
+    public void validateRegistryResultWhenEverythingIsAlright() {
         Boolean ALIVE = Boolean.TRUE;
         String Camilo = "Camilo";
         Person person = new Person(Camilo, 123, 20, Gender.MALE, ALIVE);
@@ -13,5 +13,31 @@ public class RegistryTest {
         Assert.assertEquals(RegisterResult.VALID, result);
     }
     // TODO Complete with more test cases
+    @Test
+    public void validateRegistryResultWhenAPersonIsDead() {
+        Boolean ALIVE = Boolean.FALSE;
+        String Camilo = "Camilo";
+        Person person = new Person(Camilo, 123, 20, Gender.MALE, ALIVE);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+    @Test
+    public void validateRegistryResultWhenAPersonIsUnderageToVote() {
+        Boolean ALIVE = Boolean.FALSE;
+        String Camilo = "Camilo";
+        Person person = new Person(Camilo, 123, 10, Gender.MALE, ALIVE);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+    @Test
+    public void validateRegistryResultWhenAPersonHasUnvalidAgeToVote() {
+        Boolean ALIVE = Boolean.FALSE;
+        String Camilo = "Camilo";
+        Person person = new Person(Camilo, 123, -5, Gender.MALE, ALIVE);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+
 }
 
